c846dcd35c4bbf153c8d5e2108c750e6f980a619
tekpo-sys <tekpo29@gmail.com>
2026-08-03 21:25:09 +1000

    use enironment variables for supabase keys

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://ispsmqludalykdgyykft.supabase.co'
-const supabaseKey = 'sb_publishable_t6DGe9QTtEb2Rbwmvd0Jhg_ONSE5T0R'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
